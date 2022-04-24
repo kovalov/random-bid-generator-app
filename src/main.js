5c37abec284efb901402d5615d2071f57a9c533f
kovalov <vlad.kovalov.92@gmail.com>
2022-04-24 15:07:05 +0300

    added my project

---
+++ src/main.js
@@ -0,0 +1,54 @@
+import "../src/styles.css";
+import json from "./data.json";
+
+const form = document.querySelector("[data-form]");
+const copyButton = document.querySelector('[data-button="copy"]');
+const textareaElement = document.querySelector("[data-textarea]");
+
+const toggleButtonText = () => {
+  copyButton.textContent = "Copied";
+  const copyButtonTimeout = setTimeout(() => {
+    copyButton.textContent = "Copy";
+    clearTimeout(copyButtonTimeout);
+  }, 1500);
+};
+
+const copyToClipboard = () => {
+  const copyText = textareaElement.textContent;
+  navigator.clipboard.writeText(copyText);
+  toggleButtonText();
+};
+
+const getRandomNumber = (array) => Math.floor(Math.random() * array.length);
+
+const getPhrases = (framework) => {
+  const selected = json.technology.frontend[framework];
+  const { header, main, footer } = selected;
+
+  return {
+    greeting: json.greeting[0],
+    header: header[getRandomNumber(header)],
+    main: main[getRandomNumber(main)],
+    footer: footer[getRandomNumber(footer)],
+  };
+};
+
+const showText = (phrases) => {
+  const { greeting, header, main, footer } = phrases;
+  const newLine = "\n\n";
+
+  const text = `${greeting}${newLine}${header}${newLine}${main}${newLine}${footer}`;
+  textareaElement.textContent = text;
+};
+
+form.addEventListener("change", (event) => {
+  event.preventDefault();
+
+  if (event.target.value) {
+    const framework = event.target.value;
+    const phrases = getPhrases(framework);
+    showText(phrases);
+  }
+});
+
+copyButton.addEventListener("click", copyToClipboard);
